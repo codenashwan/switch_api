f7ee11ae1aaf6819a83d873502692ae9334bfa38
codenashwan <nashwan.via.ps3@gmail.com>
2022-08-29 21:14:50 +0300

    with rest api

--- app.js
+++ app.js
@@ -3,27 +3,48 @@ const qs = require("querystring");
 
 async function checkout() {
   console.log("loading...");
-  const { data } = await axios.post(
-    "https://switch.mtf.gateway.mastercard.com/api/nvp/version/66",
-    qs.stringify({
-      apiOperation: "INITIATE_CHECKOUT",
-      apiPassword: "fb1694d371ab10ab16bee6e28e473f5b",
-      apiUsername: "merchant.TEST90006000",
-      merchant: "TEST90006000",
-      "interaction.operation": "AUTHORIZE",
-      "order.id": "12345nss",
-      "order.amount": "100.00",
-      "order.currency": "USD",
-      "order.description": "Test Order",
-    }),
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+  const token = Buffer.from(
+    `merchant.TEST90006000:fb1694d371ab10ab16bee6e28e473f5b`,
+    "utf8"
+  ).toString("base64");
+
+  await axios
+    .post(
+      "https://switch.mtf.gateway.mastercard.com/api/rest/version/64/merchant/TEST90006000/session",
+      // qs.stringify(
+      {
+        apiOperation: "CREATE_CHECKOUT_SESSION",
+        // apiPassword: "fb1694d371ab10ab16bee6e28e473f5b",
+        // apiUsername: "merchant.TEST90006000",
+        // merchant: "TEST90006000",
+        "interaction.operation": "AUTHORIZE",
+        "order.id": "22pikeme3k",
+        "order.amount": 100.0,
+        "order.currency": "USD",
+        // "order.description": "Test Order",
       },
-    }
-  );
-  console.log(data);
+      // ),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          // Authorization: `Basic ${token}`,
+        },
+        // auth: {
+        //   username: "merchant.TEST90006000",
+        //   password: "fb1694d371ab10ab16bee6e28e473f5b",
+        // },
+      }
+    )
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      console.log("done");
+    });
+  // console.log(data);
 }
 
 checkout();
-  
